ec8d07aad92171d01fdd9d647bce715cc963e360
fjbara <farahjbara1@gmail.com>
2024-06-23 17:56:50 +0100

    Create a public $ private subnets

---
+++ Project/modules/subnet/main.tf
@@ -0,0 +1,28 @@
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id            = var.vpc_id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id            = var.vpc_id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
---
+++ Project/modules/subnet/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
