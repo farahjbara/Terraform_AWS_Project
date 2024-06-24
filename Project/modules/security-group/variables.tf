05cd3dcab55e2420dbb5e626422bd30d96e646b3
fjbara <farahjbara1@gmail.com>
2024-06-24 11:12:51 +0100

    create a secutity group

---
+++ Project/modules/security-group/variables.tf
@@ -0,0 +1,49 @@
+variable "name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the security group will be created"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+variable "tags" {
+  description = "Tags for the security group"
+  type        = map(string)
+  default     = {}
+}
